e959c1f92a94eb2ec9020d04c348815faa2bec94
willcoxjp <patrick.willcox@gmail.com>
2025-03-05 12:53:40 -0500

    Update page.tsx
    Updated the examples and botname

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ import Textarea from "react-textarea-autosize";
 import Image from "next/image";
 
 const examples = [
-  "Example 1",
-  "Example 2",
-  "Example 3",
+  "Happy",
+  "Sad",
+  "Curious",
 ];
 
 export default function Chat() {
@@ -83,7 +83,7 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a fine tuned LLM.
+              Hi, I'm PWTest.
             </h1>
             <p className="text-gray-500">
               I'm part of a series of computational experiments taught by{" "}
